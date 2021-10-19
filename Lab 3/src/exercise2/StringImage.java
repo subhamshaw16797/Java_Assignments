1d1f471e14a6bac0a7d44178f01d2b7f8087a5ef
Subham Kumar Shaw <subhamshaw16797@gmail.com>
2021-10-19 23:15:47 +0530

    String

---
+++ Lab 3/src/exercise2/StringImage.java
@@ -0,0 +1,28 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class StringImage {
+
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a String that you want to mirror: ");
+		//ask for user input
+		String str = scn.nextLine();
+		System.out.println("Mirror String is: "+getImage(str));
+		scn.close();
+	}
+	
+	//logic for reverse the string
+	public static String getImage(String str) {
+		String revStr="";
+		for(int i= str.length()-1; i>=0;i--) {
+			revStr+=str.charAt(i);
+		}
+		
+		//mirror the string
+		str=str+"|"+revStr;
+		return str;
+	}
+}
---
+++ Lab 3/src/exercise3/ReplaceConsonants.java
@@ -0,0 +1,11 @@
+package exercise3;
+
+public class ReplaceConsonants {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
