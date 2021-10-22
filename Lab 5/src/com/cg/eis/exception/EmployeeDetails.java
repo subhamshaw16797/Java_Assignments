f5b7686b0c075f4fc12bed1008f0135992170b55
Subham Kumar Shaw <subhamshaw16797@gmail.com>
2021-10-22 17:55:22 +0530

    Added Exception Handling

--- Lab 5/src/com/cg/eis/exception/EmployeeDetails.java
+++ Lab 5/src/com/cg/eis/exception/EmployeeDetails.java
@@ -7,31 +7,57 @@ public class EmployeeDetails {
 	String eName;
 	int eId;
 	double eSalary;
-	String eDesignation;
 	
 	
 
-	EmployeeDetails(String eName, int eId, double eSalary, String eDesignation) {
+	EmployeeDetails(String eName, int eId, double eSalary) {
 		super();
 		this.eName = eName;
 		this.eId = eId;
 		this.eSalary = eSalary;
-		this.eDesignation = eDesignation;
 	}
+	
+	
+
+
+
+	@Override
+	public String toString() {
+		return "EmployeeDetails [eName=" + eName + ", eId=" + eId + ", eSalary=" + eSalary + ", eDesignation="
+				+"]";
+	}
+
+
 
 
 
 	public static void main(String[] args) {
 		
 		Scanner scn= new Scanner(System.in);
-		System.out.println("");
+		
+		System.out.println("Enter Your Full Name: ");
 		String name= scn.nextLine();
+		
+		System.out.println("Enter Your Employee ID: ");
 		int id= scn.nextInt();
+		
+		
+		System.out.println("Enter Your Salary: ");
 		double salary= scn.nextDouble();
-		String desig= scn.nextLine();
 		
+		try {
+			if(salary<3000) {
+				throw new InsufficientAmountException("Try to work hard and get pay more..");
+			}else {
+				EmployeeDetails emp1= new EmployeeDetails(name, id, salary);
+				System.out.println(emp1);
+			}
+			
+			
+		} catch (InsufficientAmountException iae) {
+			System.out.println("Good Luck");
+		}
 		
-//		EmployeeDetails emp1= new EmployeeDetails();
 
 	}
 
---
+++ Lab 5/src/com/cg/eis/exception/InsufficientAmountException.java
@@ -0,0 +1,8 @@
+package com.cg.eis.exception;
+
+public class InsufficientAmountException extends Exception {
+	
+	public InsufficientAmountException(String s) {
+		System.out.println(s);
+	}
+}
