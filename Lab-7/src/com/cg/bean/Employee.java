6ae60aaf72d023b506adafaa7816219a38db62ad
Subham Kumar Shaw <subhamshaw16797@gmail.com>
2021-10-28 21:02:57 +0530

    Added Layered Architecture

---
+++ Lab-7/src/com/cg/bean/Employee.java
@@ -0,0 +1,41 @@
+package com.cg.bean;
+
+public class Employee {
+	
+	public int id;
+	public String name;
+	public double salary;
+	public String designation;
+	public String insuranceScheme;
+	
+	// Creating method to check eligibility
+	public String getInsuranceScheme() {
+		insuranceScheme="No Scheme";
+		if((salary>5000 && salary<20000) && designation.equalsIgnoreCase("System Associate")) {
+			insuranceScheme="Scheme C";
+		}
+		
+		else if ((salary>=20000 && salary<40000) && designation.equalsIgnoreCase("Programmer")) {
+			insuranceScheme= "Scheme B";
+		}
+		
+		else if (salary>=40000 && designation.equalsIgnoreCase("Manager")) {
+			insuranceScheme= "Scheme A";
+		}
+		
+		else if (salary<5000 && designation.equalsIgnoreCase("Clerk")) {
+			insuranceScheme= "No Scheme";
+		}
+		
+		return insuranceScheme;
+	}
+	
+	// toString method
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", designation=" + designation
+				+ ", insuranceScheme=" + insuranceScheme + "]";
+	}
+	
+	
+}
---
+++ Lab-7/src/com/cg/pl/EmployeeDemo.java
@@ -0,0 +1,16 @@
+package com.cg.pl;
+
+import java.util.Scanner;
+
+import com.cg.bean.Employee;
+
+public class EmployeeDemo extends Employee {
+
+	public static void main(String[] args) {
+		
+		Scanner scn= new Scanner(System.in);
+		
+
+	}
+
+}
